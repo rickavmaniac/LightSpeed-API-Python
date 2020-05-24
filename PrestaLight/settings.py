3aaee92df586f8948a04fa02f0c46c7154dde4f5
Eric <rickavmaniac@gmail.com>
2020-05-24 19:32:26 -0400

    azure-2

--- PrestaLight/settings.py
+++ PrestaLight/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'LightSpeed.apps.LightspeedConfig',
+    'LightSpeed',
+    "PrestaLight",
 ]
 
 MIDDLEWARE = [
